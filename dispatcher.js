1f03132735b0c4425c32e41bb8a37bd60c1805ba
Zhivko Siderov <zhivko.siderov@gmail.com>
2016-02-16 13:05:24 +0000

    Displays average price and exported the functions

--- dispatcher.js
+++ dispatcher.js
@@ -1,19 +1,26 @@
 var dispatcher = require("httpdispatcher");
+var app = require('./priceQuantity.js');
 var request = require("request");
 const URL= "https://openapi.etsy.com/v2/listings/active?api_key=j3k97n7im670ejskv8ij7nip";
 
 dispatcher.onGet("/", function(req, res) {
   request(URL, function(error, response, body) {
     var data = JSON.parse(body);
-    var text = data.results;
+    var results = data.results;
+    results = app.averagePrice(results)
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end(JSON.stringify(text));
+    res.end(JSON.stringify(results));
   });
 });
 
 dispatcher.onGet("/price", function(req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Price Page');
+  request(URL, function(error, response, body) {
+    var data = JSON.parse(body);
+    var results = data.results;
+    results = app.averagePrice(results)
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end(JSON.stringify(results));
+  });
 });
 
 dispatcher.onGet("/quantity", function(req, res) {
--- priceQuantity.js
+++ priceQuantity.js
@@ -1,42 +1,36 @@
-// mock data
+var app = {};
 
 const currencyTable = { USD: 1.0, GBP: 0.69052, EUR: 0.89445, CAD: 1.3823, AUD: 1.3975 };
 
-const results = [ { price: 10, currency_code: 'USD', quantity: 15 },
-                  { price: 20, currency_code: 'GBP', quantity: 10 },
-                  { price: 30, currency_code: 'EUR', quantity: 7 }
-                ]
 
-// helper functions
-
-function convertToUSD(price, code) {
+function convertToUSD (price, code) {
   return price / currencyTable[code];
 }
 
-function USDprice(listing) {
+function USDprice (listing) {
   return convertToUSD(listing.price, listing.currency_code);
 }
 
-function prices(listings) {
+function prices (listings) {
   return listings.map(USDprice);
 }
 
-function quants(listings) {
+function quants (listings) {
   return listings.map(listing => listing.quantity);
 }
 
-function averageValue(listings, valueFunc) {
+function averageValue (listings, valueFunc) {
   const sum = valueFunc(listings).reduce((a, b) => a + b);
   return sum / (listings.length);
 }
 
-function maxValueAndListing(listings, valueFunc) {
+function maxValueAndListing (listings, valueFunc) {
   return listings.reduce(((max, listing) =>
     valueFunc(listing) > valueFunc(max) ? listing : max
   ), listings[0]);
 }
 
-function minValueAndListing(listings, valueFunc) {
+function minValueAndListing (listings, valueFunc) {
   return listings.reduce(((min, listing) =>
     valueFunc(listing) < valueFunc(min) ? listing : min
   ), listings[0]);
@@ -44,40 +38,32 @@ function minValueAndListing(listings, valueFunc) {
 
 // top level functions
 
-function averagePrice(listings) {
+app.averagePrice = function (listings) {
   return { average_price_in_USD: averageValue(listings, prices) };
 }
 
-function averageQuantity(listings) {
+app.averageQuantity = function (listings) {
   return { average_quantity: averageValue(listings, quants) };
 }
 
-function maxPriceAndListing(listings) {
+app.maxPriceAndListing = function (listings) {
   const max = maxValueAndListing(listings, USDprice);
   return { max_price_in_USD: USDprice(max), listing: max };
 }
 
-function maxQuantityAndListing(listings) {
+app.maxQuantityAndListing = function (listings) {
   const max = maxValueAndListing(listings, listing => listing.quantity);
   return { max_quantity: max.quantity, listing: max };
 }
 
-function minPriceAndListing(listings) {
+app.minPriceAndListing = function (listings) {
   const min = minValueAndListing(listings, USDprice);
   return { min_price_in_USD: USDprice(min), listing: min };
 }
 
-function minQuantityAndListing(listings) {
+app.minQuantityAndListing = function (listings) {
   const min = minValueAndListing(listings, listing => listing.quantity);
   return { min_quantity: min.quantity, listing: min };
 }
 
-// spit out results
-
-console.log(results);
-console.log(averagePrice(results));
-console.log(averageQuantity(results));
-console.log(maxPriceAndListing(results));
-console.log(maxQuantityAndListing(results));
-console.log(minPriceAndListing(results));
-console.log(minQuantityAndListing(results));
+module.exports = app;
