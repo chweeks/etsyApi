a8779ce6b8a22f073d6b0963983b18f1f1af8fb4
chweeks <chrisharryweeks@gmail.com>
2016-02-16 12:22:49 +0000

    returns results array on route

--- web_server.js
+++ web_server.js
@@ -1,8 +1,10 @@
 var http = require('http');
+var request = require("request");
 var dispatcher = require('httpdispatcher');
 const PORT=8080;
+const URL= "https://openapi.etsy.com/v2/listings/active?api_key=j3k97n7im670ejskv8ij7nip";
 
-function handleRequest(request, response){
+function handleRequest(request, response) {
   try {
     console.log(request.url);
     dispatcher.dispatch(request, response);
@@ -12,8 +14,12 @@ function handleRequest(request, response){
 }
 
 dispatcher.onGet("/", function(req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Home Page');
+  request(URL, function(error, response, body) {
+    var data = JSON.parse(body);
+    var text = data.results;
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end(JSON.stringify(text));
+  });
 });
 
 dispatcher.onGet("/price", function(req, res) {
